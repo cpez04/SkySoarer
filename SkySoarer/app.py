d2ff0f4374daad6ab800b1c5761f23f9e681b44a
hermanlxy <112781429+hermanlxy@users.noreply.github.com>
2022-11-23 06:42:03 +0000

    Co-authored-by: Christopher Perez <cpez04@users.noreply.github.com>

--- SkySoarer/app.py
+++ SkySoarer/app.py
@@ -20,8 +20,8 @@ Session(app)
 db = SQL("sqlite:///skysoarer.db")
 
 # Make sure API key is set
-# if not os.environ.get("API_KEY"):
-    # raise RuntimeError("API_KEY not set")
+#if not os.environ.get("API_KEY"):
+    #raise RuntimeError("API_KEY not set")
 
 @app.after_request
 def after_request(response):
