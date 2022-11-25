ef4ec84a9a6288b31a224684058f093f8fe2d0ae
Chris Perez <cubanchris1@gmail.com>
2022-11-24 23:47:18 -0500

    adfasdfadsfa

--- SkySoarer/app.py
+++ SkySoarer/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config['TEMPLATES_AUTO_RELOAD'] = True #Hello
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 Session(app)
 
 # Configure CS50 Library to use database
