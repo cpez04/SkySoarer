c4df4fafda48ee097813de09e3a5a25655d306c1
Christopher Perez <cubanchris1@gmail.com>
2022-11-30 14:39:51 -0500

    nearby function

---
+++ Source/nearby.py
@@ -0,0 +1,16 @@
+import requests
+import json
+#gets list of nearby airports (we need to parse database to only get name and distance from user)
+params = {
+  'api_key': 'c6f24eaf-a7e1-412b-8fdc-f0ca0194c440',
+  'lat': '42.378021',
+  'lng': '-71.116392',
+  'distance': '30'
+}
+method = 'nearby'
+api_base = 'http://airlabs.co/api/v9/'
+api_result = requests.get(api_base+method, params)
+api_response = api_result.json()["response"]["airports"][2]['name']
+
+print(api_response)
+
