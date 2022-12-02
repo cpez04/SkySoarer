5a9cbac6c302733bf105deb903a94f21878f0c3b
Christopher Perez <cubanchris1@gmail.com>
2022-12-02 03:59:18 -0500

    hackathon - 4am

--- Source/app.py
+++ Source/app.py
@@ -204,7 +204,46 @@ def track():
 def best():
     """Search for best flights"""
     if request.method == "POST":
+        url = "https://skyscanner44.p.rapidapi.com/search"
 
-       return render_template("bested.html")
-    # Redirect user to login form
+        querystring = {"adults":"1","origin":request.form.get("origin"),"destination":request.form.get("destination"),"departureDate":request.form.get("departureDate"),"currency":"USD"}
+
+        headers = {
+            "X-RapidAPI-Key": "49e1e79da8msh462c078111aa514p12ee18jsn10c23c73dcc6",
+            "X-RapidAPI-Host": "skyscanner44.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        #api_response = response.json()['itineraries']['buckets'][0]['items'][0]['legs'][0]['segments'][0]['operatingCarrier']['name']#gets name of operating airlines for 0th row
+        api_response = response.json()['itineraries']['buckets'][0]['items']
+
+        keys = []
+        values = []
+        dict = {}
+        length = len(api_response)
+
+        # return apology if no best flight found
+        if length == 0:
+            return apology("No best flight found", 400)
+
+        # create a list of names
+        for i in range(length):
+            tmp = []
+            keys.append(api_response[i]['legs'][0]['segments'][0]['operatingCarrier']['name']) 
+
+            # insert formatted price, stop count, flight number in tmp
+            tmp.append(api_response[i]['price']['formatted'])
+            tmp.append(api_response[i]['legs'][0]['stopCount'])
+            tmp.append(api_response[i]['legs'][0]['segments'][0]['flightNumber'])
+            tmp.append(api_response[i]['legs'][0]['durationInMinutes'])
+
+            # insert tmp into values
+            values.append(tmp)
+
+        # set up dict
+        for i in range(length):
+            dict[keys[i]] = values[i]
+
+        return render_template("bested.html", dict=dict, adults=request.form.get("numAdults"))
     return render_template("best.html")
--- Source/prices.py
+++ Source/prices.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://skyscanner44.p.rapidapi.com/search"
 
-querystring = {"adults":"1","origin":"EWR","destination":"KUL","departureDate":"2022-12-11","currency":"USD"}
+querystring = {"adults":"2","origin":"EWR","destination":"KUL","departureDate":"2022-12-11","currency":"USD"}
 
 headers = {
 	"X-RapidAPI-Key": "49e1e79da8msh462c078111aa514p12ee18jsn10c23c73dcc6",
